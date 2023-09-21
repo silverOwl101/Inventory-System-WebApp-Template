f6e77c48f6ba1252f62a516f9f8c0668ff46615b
exxxcube <johnexxxcube@gmail.com>
2023-09-21 13:57:50 +0800

    Project Uploaded

---
+++ Inventory System Template Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventory_System_Template_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
