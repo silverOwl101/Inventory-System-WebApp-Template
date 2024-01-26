a975381a5d3131f22c6160ea41abc6f664e74c50
exxxcube <johnexxxcube@gmail.com>
2024-01-26 18:05:25 +0800

    Project Updated

--- Inventory System Template Web App/Controllers/HomeController.cs
+++ Inventory System Template Web App/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Inventory_System_Template_Web_App.Models;
+﻿using Inventory_System_Template_Web_App.Data;
+using Inventory_System_Template_Web_App.Models;
+using Inventory_System_Template_Web_App.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,17 +10,49 @@ namespace Inventory_System_Template_Web_App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDBContext _applicationDBContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager,
+                              SignInManager<AppUser> signInManager, ApplicationDBContext applicationDBContext)
         {
             _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _applicationDBContext = applicationDBContext;
         }
 
         public IActionResult Index()
         {            
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
 
+            var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+            if (user != null)
+            {
+                bool passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user,loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Account");
+                    }
+                    TempData["Error"] = "Wrong credentials. Please, try again";
+                    return View(loginViewModel);
+                }                
+            }
+            TempData["Error"] = "Wrong credentials. Please, try again";
+            return View(loginViewModel);
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Inventory System Template Web App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_System_Template_Web_App.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public required string Email { get; set; }
+        [DataType(DataType.Password)]
+        public required string  Password { get; set; }
+    }
+}
