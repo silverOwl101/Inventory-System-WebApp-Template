1b5f50623466ca8d7270a28c078855b1490592c1
exxxcube <johnexxxcube@gmail.com>
2024-02-21 20:09:15 +0800

    Project Updated

--- Inventory System Template Web App/Controllers/RolesController.cs
+++ Inventory System Template Web App/Controllers/RolesController.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Inventory_System_Template_Web_App.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Inventory_System_Template_Web_App.Controllers
 {
     public class RolesController : Controller
     {
-        [Authorize(Roles = "admin")]
-        public IActionResult Index()
+        private readonly ISystemUserRepository _systemUserRepository;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(ISystemUserRepository systemUserRepository, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            _systemUserRepository = systemUserRepository;
+            _roleManager = roleManager;
+        }
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _systemUserRepository.GetAllRoles();
+            return View(roles);
         }
     }
 }
--- Inventory System Template Web App/obj/Debug/net8.0/Inventory System Template Web App.AssemblyInfo.cs
+++ Inventory System Template Web App/obj/Debug/net8.0/Inventory System Template Web App.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory System Template Web App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b2cea5691de1090d202293bfa2b5d58d0992bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85fd6fb32a408660d9304fafa24c791c3f08ac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory System Template Web App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory System Template Web App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
